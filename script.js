202142c6c0ab075caa0bf711749a22222fee44a8
Fabian Ursache(uie64738) <fabian-corneliu.ursache@continental-corporation.com>
2024-06-17 13:29:01 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let userButton = document.querySelector("#check");
+let message = document.querySelector(".message");
+let currentScoreValue = document.querySelector("#currentScoreValue");
+let highScoreValue = document.querySelector("#highScoreValue"); 
+
+
+console.log(highScoreValue);
+
+let checkNumber = () =>{
+
+}
+
+userButton.addEventListener("click", ()=>{
+    console.log("ok");
+})
